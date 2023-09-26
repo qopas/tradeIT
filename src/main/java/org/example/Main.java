ba1843e5873dd1dff32b236a3e9519a1e36ff0b9
Felicia <felicianovac4@gmail.com>
2023-09-26 20:23:43 +0300

    Upload the database changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,24 @@
 package org.example;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
+@RestController
 public class Main {
-    public static void main(String[] args) throws  IOException{
+    public static void main(String[] args) throws  IOException {
         SpringApplication.run(Main.class, args);
+    }
+    @GetMapping
+    public String smt(){
+        return "ceva";
     }
 }
